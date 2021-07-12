f3093aa179869cb40a77cdd67dc4c4a10508d89e
LaughingLeader <laughingleader@gmail.com>
2021-07-12 16:36:06 -0500

    Refining potion override (skip actual potions/food), write overwritten stats to a log

--- Mods/ResistanceRemover_fac8042a-bf84-4a8a-9ccd-433a0305a861/Story/RawFiles/Lua/BootstrapShared.lua
+++ Mods/ResistanceRemover_fac8042a-bf84-4a8a-9ccd-433a0305a861/Story/RawFiles/Lua/BootstrapShared.lua
@@ -22,7 +22,7 @@ function StatsOverrider:OverrideStat(id, statAttributes, syncStat)
 	local overwritten = false
 	for property,_ in pairs(statAttributes) do
 		local value = Ext.StatGetAttribute(id, property)
-		if value > 0 then
+		if type(value) == "number" and value > 0 then
 			if not syncStat then
 				Ext.StatSetAttribute(id, property, 0)
 			else
@@ -36,12 +36,22 @@ function StatsOverrider:OverrideStat(id, statAttributes, syncStat)
 	return overwritten
 end
 
+function StatsOverrider:CanOverridePotion(id)
+	if self.Ignored[id] then
+		return false
+	end
+	if Ext.StatGetAttribute(id, "IsConsumable") == "Yes" or Ext.StatGetAttribute(id, "IsFood") == "Yes" then
+		return false
+	end
+	return true
+end
+
 ---@param entries string[]
 function StatsOverrider:OverridePotions(entries, syncStat)
 	local overwrites = {}
 	local statAttributes = self.Attributes.Potion
 	for _,id in pairs(entries) do
-		if not self.Ignored[id] then
+		if self:CanOverridePotion(id) then
 			if self:OverrideStat(id, statAttributes, syncStat) then
 				overwrites[#overwrites+1] = id
 			end
@@ -50,18 +60,21 @@ function StatsOverrider:OverridePotions(entries, syncStat)
 	local count = #overwrites
 	if count > 0 then
 		Ext.Print(string.format("[%s:OverridePotions] Changed resistances in (%s) stats.", self.PrintID, count))
+
+		local b,err = xpcall(function()
+			table.sort(overwrites)
+			local str = ""
+			for i=1,count do
+				str = string.format("%s%s%s", str, overwrites[i], i < count and "\n" or "")
+			end
+			local fileName = string.format("Logs/ResistanceRemover_Overwrites_Potion_%s.txt", Ext.IsClient() and "Client" or "Server")
+			Ext.SaveFile(fileName, str)
+		end, debug.traceback)
+		if not b then
+			Ext.PrintError(err)
+		end
 	else
 		Ext.Print(string.format("[%s:OverridePotions] No stats needed changing (already parsed?).", self.PrintID, count))
-	end
-	if Ext.IsDeveloperMode() then
-		local str = ""
-		for i=1,count do
-			str = str + overwrites[i]
-			if i < count then
-				str = str + "\n"
-			end
-		end
-		Ext.SaveFile(string.format("Logs/ResistanceRemover_Overwrites_Potion_%s.txt", Ext.IsClient() and "Client" or "Server"), str)
 	end
 end
 
